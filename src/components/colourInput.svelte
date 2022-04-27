75d8114cd2213805c6c7fce2f3715207387afcd2
Surjith S M <surjithctly@gmail.com>
2022-04-27 21:38:38 +0530

    ux improvements

--- src/components/colourInput.svelte
+++ src/components/colourInput.svelte
@@ -1,5 +1,42 @@
 <script>
   import { colour as userColour } from "../scripts/colourStore";
+  import { isValidColour } from "../scripts/isValidColour";
+  let clipAccess = true;
+  const onFocus = () => {
+    navigator.clipboard
+      .readText()
+      .then(async (text) => {
+        const validClipboard = isValidColour(text);
+
+        const hexMatchNohash = new RegExp(
+          "^((?:[0-9a-f]{3}){1,2})$",
+          "i"
+        );
+
+        if (validClipboard) {
+          const hexMatchNohash = new RegExp(
+            "^((?:[0-9a-f]{3}){1,2})$",
+            "i"
+          );
+          if (hexMatchNohash.test(text)) {
+            $userColour = `#${text}`;
+          } else {
+            $userColour = text;
+          }
+          // reset to empty
+          try {
+            await navigator.clipboard.writeText("");
+          } catch (err) {
+            console.error("Cannot reset clipboard", err);
+          }
+        }
+        console.log("Pasted content: ", text);
+      })
+      .catch((err) => {
+        clipAccess = false;
+        console.error("Failed to read clipboard contents: ", err);
+      });
+  };
 </script>
 
 <section class="mt-2">
@@ -7,21 +44,27 @@
     Enter a colour, get a Tailwind colour name
   </p>
 
-  <label class="block mt-0 text-gray-700">
+  <div class="block mt-0 text-gray-700">
     Type a hex colour code like
     <code>#FFFFFF</code>
     or
     <code>#ed3939</code>
 
-    <input
-      placeholder="#FFFFFF"
-      class="block w-48 mt-4 uppercase rounded shadow focus:ring-cyan-800"
-      id="colour"
-      type="text"
-      title="Type a hex colour code"
-      bind:value={$userColour}
-    />
-  </label>
+    {#if !clipAccess}
+      <div class="text-yellow-600 my-3">
+        Enable Clipboard Access for easy pasting next time
+      </div>
+    {/if}
+  </div>
+  <input
+    placeholder="#FFFFFF"
+    class="block w-48 mt-4 uppercase rounded shadow focus:ring-cyan-800"
+    id="colour"
+    type="text"
+    title="Type a hex colour code"
+    on:focus={onFocus}
+    bind:value={$userColour}
+  />
 
   <label class="block mt-4 text-gray-700">
     Or select a colour from this colour picker
--- src/scripts/isValidColour.js
+++ src/scripts/isValidColour.js
@@ -1,7 +1,8 @@
 const isValidColour = (input) => {
   const hexMatch = new RegExp("^#((?:[0-9a-f]{3}){1,2})$", "i");
+  const hexMatchNohash = new RegExp("^((?:[0-9a-f]{3}){1,2})$", "i");
 
-  return hexMatch.test(input);
+  return hexMatch.test(input) || hexMatchNohash.test(input);
 };
 
 export { isValidColour };
