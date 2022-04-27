9ba14d2b26bfebbdcfada884b7575befc5f71760
Surjith S M <surjithctly@gmail.com>
2022-04-27 19:51:08 +0530

    design changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,16 +11,20 @@
   <title>Find the nearest Tailwind Colour</title>
 </svelte:head>
 
-<header class="p-4 mt-1 -ml-4 -mr-4 bg-cyan-800 sm:rounded">
-  <h1 class="leading-none text-cyan-50">Find the nearest Tailwind colour</h1>
-  <p class="mt-2 text-cyan-100">
+<header
+  class="p-4 mt-3 pb-5  text-indigo-800 sm:rounded text-center border-b border-gray-200"
+>
+  <h1 class="leading-none text-indigo-800 font-bold tracking-tight">
+    Tailwind colour
+  </h1>
+  <p class="mt-2 text-gray-500">
     A tool to find the nearest colour from
     <a href="https://tailwindcss.com/docs/customizing-colors">
       the Tailwind colour palette</a
     >
   </p>
-  <p class="mt-2 text-sm text-cyan-100" style="">
-    For Tailwind CSS v3.0 (beta)
+  <p class="mt-2 text-sm text-indigo-800" style="">
+    For Tailwind CSS v3.0
   </p>
 </header>
 
@@ -37,15 +41,16 @@
 </main>
 
 <footer
-  class="p-4 mt-8 -ml-4 -mr-4 sm:rounded text-cyan-50 bg-cyan-800 border-top-4"
+  class="p-4 mt-8 -ml-4 -mr-4 sm:rounded text-gray-400  border-top-4 text-center text-xs"
 >
   <p class="mt-0">
-    Made by
-    <a href="https://edjohnsonwilliams.co.uk/">Ed Johnson-Williams</a>
+    Powered by <a href="https://web3templates.com/">Web3Templates</a>
   </p>
+
   <p>
-    <a href="https://github.com/edjw/find-nearest-tailwind-colour"
-      >See the code for this website</a
+    <a href="https://edjohnsonwilliams.co.uk/">Author</a> |
+    <a href="https://github.com/web3templates/tailwind-color"
+      >Github</a
     >
   </p>
 </footer>
